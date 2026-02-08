4b6652c9d9abf16114ffeaf5cdd252f8296005a3
Toheed Sahar <gmalyktoheed@gmail.com>
2026-02-08 16:15:39 +0500

    Build: Complete synchronization of project source, dependencies, and build configurations for Vercel

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/CRM-System",
+  trailingSlash: true,
+  output: "standalone",
   serverExternalPackages: ["@prisma/client", "bcryptjs"],
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
